c28463cbd8bf4f55d2ff4531bf0714986506d07f
Vincent Kamusella <kx338@stud.uni-heidelberg.de>
2025-12-12 09:38:48 +0100

    Review.java

--- domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
+++ domain/src/main/java/de/seuhd/campuscoffee/domain/model/objects/Review.java
@@ -1,8 +1,12 @@
 package de.seuhd.campuscoffee.domain.model.objects;
 
-import lombok.Builder;
+import java.time.LocalDateTime;
+
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
+
+import lombok.Builder;
+
 
 /**
  * Domain record that stores a review for a point of sale.
@@ -11,7 +15,10 @@ import org.jspecify.annotations.Nullable;
 @Builder(toBuilder = true)
 public record Review(
         @Nullable Long id, // null when the review has not been created yet
-        //TODO: Implement review domain model.
+        @Nullable LocalDateTime createdAt, //can be Null when a new review is created
+        @Nullable LocalDateTime updatedAt, //can be Null when a new review is created
+        @NonNull Long posId, 
+        @NonNull Long authorId, 
         @NonNull Integer approvalCount, // is updated by the domain module
         @NonNull Boolean approved // is determined by the domain module
 ) implements DomainModel<Long> {
